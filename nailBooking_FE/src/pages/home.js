7cb38ded6f04aa6e10bd8b9d20219f3a3f537961
echang1 <echang1@ftdm.com.tw>
2021-02-17 17:03:55 +0800

    update calendat event

--- nailBooking_FE/src/pages/home.js
+++ nailBooking_FE/src/pages/home.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Header from "../header/header.js";
-import "../place/place.css";
-// import Place from "../place/place.js";
 
 class Home extends React.Component {
     constructor(props) {
